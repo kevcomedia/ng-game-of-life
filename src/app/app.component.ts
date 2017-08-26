1bf8f7ced5f616c96818425c61cc24aa4795028e
Kev Comedia <kevcomedia@gmail.com>
2017-08-26 09:39:26 +0800

    Make random cells in grid alive on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GameOfLifeService } from './game-of-life.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  rows = 10;
+  cols = 10;
+
+  constructor(private gameOfLifeService: GameOfLifeService) {}
+
+  ngOnInit() {
+    this.gameOfLifeService.initialize(this.rows, this.cols);
+    this.gameOfLifeService.randomizeCellStates();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameOfLifeGridComponent } from './game-of-life-grid.component';
+import { GameOfLifeService } from './game-of-life.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameOfLifeGridComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [GameOfLifeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
